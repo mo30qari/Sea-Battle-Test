b83aaa467e02e2aabc64c76b0667279c88257ceb
mo30qari <mo30qari@gmail.com>
2021-01-14 17:24:24 +0330

    Corrected <JoinToRoomReq> <JoinToRoomRes> & <GameStart> and added <SetShipsReq> & <SetShipsRes>. These changes should be checked out.

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-let WebSocketServer = require('ws').Server, wss = new WebSocketServer({port: 8070});
-let PLAYERS = [];
-let ROOMS = [];
-let CAPACITY = 2;
-let timeout = 8000;
-let timer, startTime;
+let WebSocketServer = require('ws').Server, wss = new WebSocketServer({ port: 8070 })
+let PLAYERS = []
+let ROOMS = []
+let CAPACITY = 2
+let timeout = 8000000
+let timer, startTime
 
-function Room(id, capacity, safeSquares, data) {
-    this.id = id;
-    this.capacity = capacity;
-    this.safeSquares = safeSquares;
-    this.players = [];
-    this.data = data;
+function Room(id, capacity, data) {
+    this.id = id
+    this.capacity = capacity
+    this.players = []
+    this.data = data
 }
 
 function Player(ws, id, roomId, num, ready, absence, deleted) {
-    this.ws = ws;
-    this.id = id;
-    this.roomId = roomId;
-    this.num = num;
-    this.ready = ready;
-    this.absence = absence;
-    this.deleted = deleted;
+    this.ws = ws
+    this.id = id
+    this.roomId = roomId
+    this.num = num
+    this.ready = ready
+    this.absence = absence
+    this.deleted = deleted
 }
 
 wss.SendDataToRoom = function broadcast(toRoomID, data, fromPlayer) {
@@ -29,54 +28,55 @@ wss.SendDataToRoom = function broadcast(toRoomID, data, fromPlayer) {
 
         wss.clients.forEach(function each(client) {
 
-            let player = PLAYERS.find(e => (e.ws === client) && (e.roomId === toRoomID));
+            let player = PLAYERS.find(e => (e.ws === client) && (e.roomId === toRoomID))
 
             if (player && (player !== fromPlayer || fromPlayer === null) && player.ready && !player.deleted) {
-                client.send(JSON.stringify(data));
+                client.send(JSON.stringify(data))
             }
-        });
+        })
 
     } else {
 
-        wss.client.send("Unauthorized Request!");
+        wss.client.send("Unauthorized Request!")
 
     }
-};
+}
 
 wss.on('connection', function (ws, request, client) {
 
-    print("Opened!");
+    print("Opened!")
 
     ws.on('message', function (message) {
 
-        print(message);
+        print(message)
 
-        let msg = JSON.parse(message);
+        let msg = JSON.parse(message)
 
         if (msg.__Type === "JoinToRoomReq") {
 
-            let room = ROOMS.find(e => e.id === msg.RoomID);
+            let room = ROOMS.find(e => e.id === msg.RoomID)
 
             if (room) {
 
-                let player = room.players.find(e => e.id === msg.PlayerID);
+                let player = room.players.find(e => e.id === msg.PlayerID)
 
+                //The old player should not send <JoinToRoomReq>
                 if (player) {
 
                     //Before, The disconnected user could reconnect from here.
-                    //but now the codes of that action migrated to the <PlayerBackReq>
-                    ws.send("The player Already Exists!");
+                    //But now the codes of that action migrated to the <PlayerBackReq>
+                    ws.send("The player Already Exists!")
 
+                    //New player
                 } else {
 
                     if (room.capacity > room.players.length) {
 
-                        let player = new Player(ws, msg.PlayerID, msg.RoomID, room.players.length + 1, 1, 0, 0);
-                        0
+                        let player = new Player(ws, msg.PlayerID, msg.RoomID, room.players.length + 1, 1, 0, 0)
 
-                        room.players.push(player);
+                        room.players.push(player)
 
-                        PLAYERS.push(player);
+                        PLAYERS.push(player)
 
                         if (player) {
 
@@ -85,20 +85,20 @@ wss.on('connection', function (ws, request, client) {
                                 "Settings": {
                                     "Capacity": room.capacity
                                 },
-                                "SafeSquares": room.safeSquares,
                                 "PlayerNumber": player.num,
                                 "Player": {
-                                    "Name": "Ali",
+                                    "Nickname": "Ali",
                                     "Avatar": 254
                                 }
-                            }));
+                            }))
 
                         } else {
 
-                            print("Error: An Error Has Occurred.");
+                            print("Error: An Error Has Occurred.")
 
                         }
 
+                        //All the players joined
                         if (room.capacity === room.players.length) {
 
                             wss.SendDataToRoom(room.id, {
@@ -113,78 +113,82 @@ wss.on('connection', function (ws, request, client) {
                                         "Avatar": ""
                                     },
                                 ]
-                            }, null);
+                            }, null)
 
-                            let turnedPlayer = room.players.find(e => e.num === 1);
-
-                            startTimer(turnedPlayer, room);
+                            //Choose the player with first turn
+                            let turnedPlayer = room.players.find(e => e.num === 1)
+                            //And startTimer for the player at the beginning of the game
+                            startTimer(turnedPlayer, room)
 
                         }
+                        //The room is full
                     } else {
-
-                        print(" The Room is Full!");
-
+                        print(" The Room is Full!")
                     }
 
                 }
 
-
+                //If the Room doesn't exist
             } else {
 
-                let room = new Room(msg.RoomID, CAPACITY, 1, {
+                let room = new Room(msg.RoomID, CAPACITY, {
                     "__Type": "RoomDataReq",
                     "Turn": 1,
-                    "Dice": 0,
                     "GameState": null
-                });
+                })
 
-                ROOMS.push(room);
+                ROOMS.push(room)
 
-                let player = new Player(ws, msg.PlayerID, msg.RoomID, room.players.length + 1, 1, 0, 0);
-
-                room.players.push(player);
-
-                PLAYERS.push(player);
+                //The player is the room creator
+                let player = new Player(ws, msg.PlayerID, msg.RoomID, room.players.length + 1, 1, 0, 0)
+                room.players.push(player)
+                PLAYERS.push(player)
 
                 if (player) {
-
                     ws.send(JSON.stringify({
                         "__Type": "JoinToRoomRes",
                         "Settings": {
                             "Capacity": room.capacity
                         },
-                        "SafeSquares": room.safeSquares,
                         "PlayerNumber": player.num,
                         "Player": {
                             "Name": "Ali",
                             "Avatar": 254
                         }
-                    }));
-
+                    }))
                 } else {
-
-                    print("Error: An Error Has Occurred.");
-
+                    print("Error: An Error Has Occurred.")
                 }
 
             }
         }
 
-        else if (msg.__Type === "DiceRolledReq") {
+        else if (msg.__Type === "SetShipsReq") {
 
-            let player = PLAYERS.find(e => e.ws === ws);
+            let player = PLAYERS.find(e => e.ws === ws)
 
             if (player && !player.deleted) {
 
-                let room = ROOMS.find(e => e.id === player.roomId);
+                let room = ROOMS.find(e => e.id === player.roomId)
 
                 if (room) {
 
-                    wss.SendDataToRoom(player.roomId, {
-                        "Dice": msg.Dice,
-                        "PlayerNumber": player.num,
-                        "__Type": "DiceRolledRes"
-                    }, player);
+                    player.ships = msg.Ships
+                    let x = 0
+                    let ships = []
+                    //If All the players sent <SetShipsRes>? If yes send them <SetShipsRes>
+                    room.players.forEach(function (ply, i) {
+                        if ("ships" in ply) {
+                            ++x
+                            ships[i] = ply.ships
+                            if (x === 2) {
+                                wss.SendDataToRoom(room.id, {
+                                    "__Type": "SetShipsRes",
+                                    "Ships": ships
+                                }, null)
+                            }
+                        }
+                    })
 
                 }
 
@@ -194,22 +198,22 @@ wss.on('connection', function (ws, request, client) {
 
         else if (msg.__Type === "RoomDataReq") {
 
-            clearTimeout(timer);
+            clearTimeout(timer)
 
-            let player = PLAYERS.find(e => e.ws === ws);
+            let player = PLAYERS.find(e => e.ws === ws)
 
             if (player && !player.deleted) {
 
-                let room = ROOMS.find(e => e.id === player.roomId);
+                let room = ROOMS.find(e => e.id === player.roomId)
 
                 if (room) {
 
-                    room.data = msg;
+                    room.data = msg
 
-                    let turnedPlayer = room.players.find(e => e.num === parseInt(msg.Turn));
+                    let turnedPlayer = room.players.find(e => e.num === parseInt(msg.Turn))
 
                     //Calculate Player Absence
-                    startTimer(turnedPlayer, room);
+                    startTimer(turnedPlayer, room)
 
                 }
 
@@ -219,37 +223,37 @@ wss.on('connection', function (ws, request, client) {
 
         else if (msg.__Type === "PlayerBackReq") {
 
-            let player = PLAYERS.find(e => e.ws === ws);
+            let player = PLAYERS.find(e => e.ws === ws)
 
             if (player && !player.deleted) {
 
                 if (player.ready) {
 
-                    let room = ROOMS.find(e => e.id === player.roomId);
+                    let room = ROOMS.find(e => e.id === player.roomId)
 
                     if (room) {
 
-                        room.data.__Type = "PlayerBackRes";
-                        room.data.ElapsedTime = (new Date().getTime() - startTime) / 1000;
-                        ws.send(JSON.stringify(room.data));
+                        room.data.__Type = "PlayerBackRes"
+                        room.data.ElapsedTime = (new Date().getTime() - startTime) / 1000
+                        ws.send(JSON.stringify(room.data))
 
                     }
                 }
                 else {
 
-                    let room = ROOMS.find(e => e.id === msg.RoomID);
+                    let room = ROOMS.find(e => e.id === msg.RoomID)
 
                     if (room) {
 
-                        let player = room.players.find(e => e.id === msg.PlayerID);
+                        let player = room.players.find(e => e.id === msg.PlayerID)
 
                         if (player) {
 
-                            player.ready = 1;
-                            player.ws = ws;
+                            player.ready = 1
+                            player.ws = ws
 
-                            room.data.__Type = "PlayerBackRes";
-                            ws.send(JSON.stringify(room.data));
+                            room.data.__Type = "PlayerBackRes"
+                            ws.send(JSON.stringify(room.data))
 
                         }
 
@@ -261,19 +265,19 @@ wss.on('connection', function (ws, request, client) {
 
                 //Duplicate
 
-                let room = ROOMS.find(e => e.id === msg.RoomID);
+                let room = ROOMS.find(e => e.id === msg.RoomID)
 
                 if (room) {
 
-                    let player = room.players.find(e => e.id === msg.PlayerID);
+                    let player = room.players.find(e => e.id === msg.PlayerID)
 
                     if (player) {
 
-                        player.ready = 1;
-                        player.ws = ws;
+                        player.ready = 1
+                        player.ws = ws
 
-                        room.data.__Type = "PlayerBackRes";
-                        ws.send(JSON.stringify(room.data));
+                        room.data.__Type = "PlayerBackRes"
+                        ws.send(JSON.stringify(room.data))
 
                     }
 
@@ -285,11 +289,11 @@ wss.on('connection', function (ws, request, client) {
 
         else if (msg.__Type === "PlayerMovedReq") {
 
-            let player = PLAYERS.find(e => e.ws === ws);
+            let player = PLAYERS.find(e => e.ws === ws)
 
             if (player && !player.deleted) {
 
-                let room = ROOMS.find(e => e.id === player.roomId);
+                let room = ROOMS.find(e => e.id === player.roomId)
 
                 if (room) {
 
@@ -298,7 +302,7 @@ wss.on('connection', function (ws, request, client) {
                         "PlayerNumber": player.num,
                         "Pawn": msg.Pawn,
                         "StepCount": msg.StepCount
-                    }, player);
+                    }, player)
 
                 }
 
@@ -308,20 +312,20 @@ wss.on('connection', function (ws, request, client) {
 
         else if (msg.__Type === "ResignReq") {
 
-            let player = PLAYERS.find(e => e.ws === ws);
+            let player = PLAYERS.find(e => e.ws === ws)
 
             if (player && !player.deleted) {
 
-                let room = ROOMS.find(e => e.id === player.roomId);
+                let room = ROOMS.find(e => e.id === player.roomId)
 
                 if (room) {
 
                     wss.SendDataToRoom(player.roomId, {
                         "__Type": "ResignUpdate",
                         "PlayerNumber": player.num
-                    }, player);
+                    }, player)
 
-                    player.deleted = 1;
+                    player.deleted = 1
 
                 }
 
@@ -329,115 +333,115 @@ wss.on('connection', function (ws, request, client) {
 
         }
 
-    });
+    })
 
     ws.on('close', function (message) {
 
-        /*print("Closed!");
+        /*print("Closed!")
 
-        let player = PLAYERS.find(e => e.ws === ws);
+        let player = PLAYERS.find(e => e.ws === ws)
 
         if (player) {
 
-            player.ready = 0;
+            player.ready = 0
 
         } else {
 
-            print("The User Doesn't Exists!");
+            print("The User Doesn't Exists!")
 
         }*/
 
         PLAYERS.forEach(function (player) {
-            player = null;
-        });
+            player = null
+        })
 
         ROOMS.forEach(function (room) {
-            room = null;
-        });
+            room = null
+        })
 
-        PLAYERS = [];
-        ROOMS = [];
+        PLAYERS = []
+        ROOMS = []
 
-        clearTimeout(timer);
+        clearTimeout(timer)
 
 
     })
 
-});
+})
 
 function print(message) {
 
-    let date = new Date();
+    let date = new Date()
     console.log("\n" +
         date.getHours() + ":" +
         date.getMinutes() + ":" +
         date.getSeconds() + ":" +
         date.getMilliseconds() + " => " +
-        message);
+        message)
 
 }
 
 function nextTurn(players, turn) {
 
-    let presentPlayers = [];
+    let presentPlayers = []
 
     players.forEach(function (player, i) {
         if (!player.deleted) {
-            presentPlayers[i] = player.num;
+            presentPlayers[i] = player.num
         }
-    });
+    })
 
-    let index = presentPlayers.indexOf(turn);
+    let index = presentPlayers.indexOf(turn)
 
     if (index >= presentPlayers.length - 1)
-        index = 0;
+        index = 0
     else
-        index++;
+        index++
 
-    return presentPlayers[index];
+    return presentPlayers[index]
 
 }
 
 function startTimer(player, room) {
     if (player && !player.deleted) {
 
-        startTime = (new Date()).getTime();
+        startTime = (new Date()).getTime()
 
         timer = setTimeout(function () {
 
-            player.absence++;
-            console.log(player.num + " Absence: " + player.absence);
+            player.absence++
+            console.log(player.num + " Absence: " + player.absence)
 
-            let newTurn = nextTurn(room.players, room.data.Turn);
+            let newTurn = nextTurn(room.players, room.data.Turn)
 
             wss.SendDataToRoom(player.roomId, {
                 "__Type": "TurnSkipped",
                 "GameState": room.data.GameState,
                 "Dice": room.data.Dice,
                 "Turn": newTurn
-            }, null);
+            }, null)
 
-            room.data.Turn = newTurn;
-            room.data.ElapsedTime = 0;
+            room.data.Turn = newTurn
+            room.data.ElapsedTime = 0
 
             if (player.absence >= 3) {
 
-                player.deleted = 1;
+                player.deleted = 1
 
                 wss.SendDataToRoom(player.roomId, {
                     "__Type": "ResignUpdate",
                     "PlayerNumber": player.num
-                }, null);
+                }, null)
 
-                console.log(player.num + ": OUT!");
+                console.log(player.num + ": OUT!")
             }
 
             //start new round for next player
-            let playerNum = nextTurn(room.players, room.data.Turn);
-            clearTimeout(timer);
-            startTimer(room.players.find(e => e.num === playerNum), room);
+            let playerNum = nextTurn(room.players, room.data.Turn)
+            clearTimeout(timer)
+            startTimer(room.players.find(e => e.num === playerNum), room)
 
-        }, timeout, player);
+        }, timeout, player)
     }
 
 }
